916e5bbbf694e8aaf98158eb2b1f81e9ad51b9f4
Akshay Rathee <akshayrathee902@gmail.com>
2024-05-15 20:20:49 +0530

    db

--- index.js
+++ index.js
@@ -1,75 +1,91 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const mongoose = require("mongodb");
+const cors = require("cors");
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://twitter_admin:XkV80JAqGQBEEkrB@cluster0.bpb775a.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const connectDB = async () => {
+  try {
+    const client = await mongoose.connect(
+      "mongodb+srv://akshayrathee:akshayrathee@cluster0.a08waa8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log(`Connected To MongoDB Database ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error in MongoDB ${error}`);
+  }
+};
+
+connectDB();
 
 async function run() {
-    try {
-        await client.connect();
-        const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
-        const userCollection = client.db("database").collection("users"); // this collection is for team-srv
+  try {
+    const postCollection = mongoose.connection
+      .db("database")
+      .collection("posts"); // this collection is for team-ekt
+    const userCollection = mongoose.connection
+      .db("database")
+      .collection("users"); // this collection is for team-srv
 
-        // get
-        app.get('/user', async (req, res) => {
-            const user = await userCollection.find().toArray();
-            res.send(user);
-        })
-        app.get('/loggedInUser', async (req, res) => {
-            const email = req.query.email;
-            const user = await userCollection.find({ email: email }).toArray();
-            res.send(user);
-        })
-        app.get('/post', async (req, res) => {
-            const post = (await postCollection.find().toArray()).reverse();
-            res.send(post);
-        })
-        app.get('/userPost', async (req, res) => {
-            const email = req.query.email;
-            const post = (await postCollection.find({ email: email }).toArray()).reverse();
-            res.send(post);
-        })
+    // get
+    app.get("/user", async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    });
+    app.get("/loggedInUser", async (req, res) => {
+      const email = req.query.email;
+      const user = await userCollection.find({ email: email }).toArray();
+      res.send(user);
+    });
+    app.get("/post", async (req, res) => {
+      const post = (await postCollection.find().toArray()).reverse();
+      res.send(post);
+    });
+    app.get("/userPost", async (req, res) => {
+      const email = req.query.email;
+      const post = (
+        await postCollection.find({ email: email }).toArray()
+      ).reverse();
+      res.send(post);
+    });
 
-        // post
-        app.post('/register', async (req, res) => {
-            const user = req.body;
-            const result = await userCollection.insertOne(user);
-            res.send(result);
-        })
+    // post
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-        app.post('/post', async (req, res) => {
-            const post = req.body;
-            const result = await postCollection.insertOne(post);
-            res.send(result);
-        })
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
 
-        // patch
-        app.patch('/userUpdates/:email', async (req, res) => {
-            const filter = req.params;
-            const profile = req.body;
-            const options = { upsert: true };
-            const updateDoc = { $set: profile };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result)
-        })
+    // patch
+    app.patch("/userUpdates/:email", async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = { $set: profile };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
 
-
-    } catch (error) {
-        console.log(error);
-    }
-} run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send('Hello from Twitter Clone!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Twitter Clone!");
+});
 
 app.listen(port, () => {
-    console.log(`Twitter clone is listening on port ${port}`)
-})
+  console.log(`Twitter clone is listening on port ${port}`);
+});
